ef62c7ca450c4edcdcdc1ec9233ff999c327cb23
ZacharyRenkema <zachary.renkema2005@gmail.com>
2024-01-05 21:40:46 -0600

    gpa calc edits

--- gpaCalculator.cpp
+++ gpaCalculator.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 
-
-
+//Letter Grade Credit Values
+int A = 4;
+int B = 3;
+int C = 2;
+int D = 1;
 
 
 int main(){
+    int courseNum; 
     //future endeavor 
-
+    std::cout << "How many courses are you taking? ";
+    std::cin >> courseNum;
 
 
     return 0;
