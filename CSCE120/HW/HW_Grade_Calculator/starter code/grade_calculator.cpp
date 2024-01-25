d288c0e5b04fdf6d920c7641de0f90fdf2555737
Zachary Renkema <zachary.renkema2005@gmail.com>
2024-01-25 02:13:57 -0600

    grade_calculator assignment (95% Score)

--- CSCE120/HW/HW_Grade_Calculator/starter code/grade_calculator.cpp
+++ CSCE120/HW/HW_Grade_Calculator/starter code/grade_calculator.cpp
@@ -40,6 +40,20 @@ int main() {
     // ONLY MAKE CHANGES WHERE THERE IS A TODO
 
     // TODO(student): declare and initialize variables that you want
+    //Variables used for processing the score.
+    double examScore = 0;
+    double hwScore = 0;
+    double readingScore = 0;
+    double engagScore = 0;
+    double lwScore = 0;
+    bool lwRead = 0;
+    
+    //These variables are the number of each category. (Used for calculating the averages)
+    int numExam = 0;
+    int numHw = 0;
+    int numReading = 0;
+    int numEngag = 0;
+    int numlw = 0;
 
     string line;
     // read one line from standard input (discards the ending newline character)
@@ -49,20 +63,46 @@ int main() {
         string category;
         double score;
         get_category_and_score(line, &category, &score);
+        
 
         // process the grade entry
         if (category == "exam") {
             // TODO(student): process exam score
+            ++numExam;
+            examScore += score;
+
         } else if (category == "final-exam") {
             // TODO(student): process final score
+            examScore += score;
+
         } else if (category == "hw") {
             // TODO(student): process hw score
+            ++numHw;
+            hwScore += score;
+
         } else if (category == "lw") {
             // TODO(student): process lw score
+            ++numlw;
+            lwRead = score;
+
+            if(lwRead == true) {
+                lwScore += 100;
+
+            }else{
+                lwScore += 0;
+
+            }
+
         } else if (category == "reading") {
             // TODO(student): process reading score
+            ++numReading;
+            readingScore += score;
+
         } else if (category == "engagement") {
             // TODO(student): process engagement score
+            ++numEngag;
+            engagScore += score;
+
         }else {
             cout << "ignored invalid input" << endl;
         }
@@ -71,18 +111,125 @@ int main() {
         getline(cin, line);
     }
 
-    // TODO(student): compute component averages
+    //Init of variables
     double exam_average = 0;
     double hw_average = 0;
     double lw_average = 0;
     double reading = 0;
     double engagement = 0;
 
+    //Init of temp variables to avoid self assignment
+    //check num values to avoid nan (divide by 0 error)
+    if(numExam == 0){
+        numExam = 1;
+
+    }else{
+        exam_average = (examScore)/(numExam + 1);
+
+    }
+
+    if(numHw == 0){
+        numHw = 1;
+
+    }else{
+        hw_average = (hwScore)/(numHw);
+
+    }
+
+    if(numlw == 0){
+        numlw = 1;
+
+    }else{
+        lw_average = (lwScore)/(numlw);
+
+    }
+
+    if (numReading == 0){
+        numReading = 1;
+
+    }else{
+        reading = ((readingScore)/(numReading)) + 15.0;      
+        double temp_read = reading;
+
+        if(reading > 100){
+            reading = 100;
+
+        }else{       
+            reading = temp_read;
+
+        }
+
+
+    }
+
+    if (numEngag == 0){
+        numEngag = 1;
+        
+    }else{
+        engagement = ((engagScore)/(numEngag)) + 15.0;
+        double temp_eng = engagement;
+
+        if(engagement > 100){
+            engagement = 100;
+
+        }else{        
+            engagement = temp_eng;
+
+        }
+
+    }
+    
+    
+    // TODO(student): compute component averages
+    /*
+    exam_average = (examScore)/(numExam + 1);
+    hw_average = (hwScore)/(numHw);
+    lw_average = (lwScore)/(numlw);
+    reading = ((readingScore)/(numReading)) + 15.0;
+    
+
+    if(reading > 100){
+        reading = 100;
+
+    }else{       
+        reading = reading;
+
+    }
+
+    double engagement = ((engagScore)/(numEngag)) + 15.0;
+
+    if(engagement > 100){
+        engagement = 100;
+
+    }else{        
+        engagement = engagement;
+
+    }
+    */
+
     // TODO(student): compute weighted total of components
-    double weighted_total = 0;
+    double weighted_total = ((exam_average*.40)+(hw_average*.40)+(lw_average*.1)+(reading*.05)+(engagement*.05));
 
     // TODO(student): compute final letter grade
-    char final_letter_grade = 'X';
+    char final_letter_grade;
+
+    if(weighted_total > 89){
+       final_letter_grade = 'A';
+
+    }
+    else if(weighted_total > 79){
+        final_letter_grade = 'B';
+
+    }else if(weighted_total > 69){
+        final_letter_grade = 'C';
+
+    }else if(weighted_total > 59){
+        final_letter_grade = 'D';
+
+    }else{
+        final_letter_grade = 'F';
+
+    }
 
     print_results(
         exam_average, hw_average, lw_average, reading, engagement,
