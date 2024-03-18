cfa112911f4fed32515c95af7f02bff18d315b5a
Zachary Renkema <zachary.renkema2005@gmail.com>
2024-03-17 19:26:34 -0500

    progress commit

--- CSCE120/LABS/LW_Strings/starter_code/functions.cpp
+++ CSCE120/LABS/LW_Strings/starter_code/functions.cpp
@@ -1,6 +1,8 @@
 # include "functions.h"
-# include <cstring> 
+//# include <cstring> 
 // add any includes
+#include <string>
+#include <iostream>
 
 using std::cout, std::cin, std::endl, std::string;
 
@@ -12,19 +14,46 @@ void deobfuscate()
 
 void wordFilter() 
 {
-    // TODO
-    
+    std::string sentence;
+    std::string filter;
 
+    std::cout << "Please enter the sentence: ";
+    std::getline(std::cin, sentence);
+
+    std::cout << "Please enter the filter word: ";
+    std::cin >> filter;
+
+    for (int i = 0; i < sentence.length(); ++i) 
+    {
+        int found = sentence.find(filter, i); 
+
+        if (found != std::string::npos) 
+        {
+            for (int j = found; j < (found + filter.length()); ++j) 
+            {
+                sentence.at(j) = '#';
+            }
+            i = found + filter.length() - 1; 
+        }
+        std::cout << i << endl;
+    }
+
+    std::cout << "Filtered sentence: " << sentence << std::endl;
 }
 
 void passwordConverter() 
 {
     // TODO
+    
     std::string password;
+    std::string original;
+    std::string modified;
     char letter;
 
     std::cout << "Please enter your text input: ";
     std::cin >> password;
+
+    original = password;
 
     for(int i = 0; i < password.length(); i++)
     {
@@ -34,26 +63,47 @@ void passwordConverter()
         {
             case 'a':
                 password.at(i) = '@';
+                modified += password.at(i);
+                break;
 
             case 'e':
                 password.at(i) = '3';
+                modified += password.at(i);
+                break;
 
             case 'i':
                 password.at(i) = '!';
+                modified += password.at(i);
+                break;
 
             case 'o':
                 password.at(i) = '0';
+                modified += password.at(i);
+                break;
 
             case 'u':
                 password.at(i) = '^';
+                modified += password.at(i);
+                break;
 
             default:
+                modified += password.at(i);
                 break;
 
         }
 
+    
     }
-    std::cout << password;
+    int i = 0;
+    int j = password.length() - 1;
+    while (i < j) {
+        std::swap(password[i], password[j]);
+        i++;
+        j--;
+    }
+    std::cout << "input: " << original << std::endl;
+    std::cout << "output: " << modified << password << std::endl;
+    
 }
 
 void wordCalculator() 
